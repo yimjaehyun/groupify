8349f41406e85c804f0208f1c485fc9cdd3a00c2
yimjaehyun <jaehyunyim1997@gmail.com>
2019-01-11 09:54:22 -0800

    added new files

--- routers/host.js
+++ routers/host.js
@@ -7,13 +7,22 @@ const scopes = ['user-read-private',
                 'user-read-email',
                 'playlist-modify-private',
                 'playlist-modify-public',
-                'user-modify-playback-state'];
+                'user-modify-playback-state',
+                'streaming',
+                'user-read-birthdate',
+                'user-read-playback-state'];
 const state = 'state';
-
+//https://github.com/spotify/web-api/issues/537
 var map = {};
+var accessTokens = {};
+var users = {};
+var votes = {};
+var duplicates = {};
+var firstPlay = {};
 
+// http://localhost:8000/host
 var spotifyApi = new SpotifyWebApi({
-  redirectUri: 'https://1e32b329.ngrok.io/host', // TODO share updated map with join so join can call add to queue with specific spotify obj
+  redirectUri: 'https://dc9805c9.ngrok.io/host', // TODO share updated map with join so join can call add to queue with specific spotify obj
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 });
@@ -29,10 +38,51 @@ router.get('/', async function(request, response, next) {
       const token = await spotifyApi.authorizationCodeGrant(code);
       await spotifyApi.setAccessToken(token.body['access_token']);
       await spotifyApi.setRefreshToken(token.body['refresh_token']);
-      //await spotifyApi.createPlaylist((await spotifyApi.getMe()).body.id, 'Groupify', { 'public' : true });
+
+      // Checks if Groupify playlist exists
+      const userId = await spotifyApi.getMe();
+      const playlists = await spotifyApi.getUserPlaylists(userId.body.id);
+      var found = false
+      var playlistId = '';
+      playlists.body.items.forEach(function(list) {
+          if(list.name === 'Groupify') {
+            playlistId = list.id;
+            found = true;
+          }
+      });
+      // not found create, else delete every song in playlist
+      if (!found) {
+        await spotifyApi.createPlaylist((await spotifyApi.getMe()).body.id, 'Groupify', { 'public' : true });
+      } else {
+        const songs = await spotifyApi.getPlaylistTracks(userId.body.id, playlistId);
+        songs.body.items.forEach(async function(song) {
+          try{
+            await spotifyApi.removeTracksFromPlaylist(userId.body.id, playlistId, [{uri : song.track.uri}]);
+          } catch(err) {
+            console.log(err)
+          }
+        })
+      }
       map[roomId] = spotifyApi;
+      users[roomId] = 0;
+      votes[roomId] = 0;
+      duplicates[roomId] = false;
+      accessTokens[roomId] = token.body['access_token'];
+      firstPlay[roomId] = false;
     }
     response.render('host.ejs', {'roomId': roomId});
+  } catch(error) {
+    console.log(error);
+  }
+});
+
+/**
+@param {roomId: 'string'}
+returns {accessToken: 'string'}
+**/
+router.post('/accessToken', async function(request, response) {
+  try {
+    response.json(accessTokens[request.body.roomId]);
   } catch(error) {
     console.log(error);
   }
@@ -45,20 +95,6 @@ removes roomId from Map and delete playlist
 router.post('/closeRoom', async function(request, response) {
   try {
     const spotifyUserApi = map[request.body.roomId];
-    const token = await spotifyUserApi.refreshAccessToken();
-    await spotifyUserApi.setAccessToken(token.body['access_token']);
-    const userId = await spotifyUserApi.getMe();
-    const playlist = await spotifyUserApi.getUserPlaylists(userId.body.id);
-    playlist.body.items.forEach(async function(list) {
-      try {
-        if(list.name === 'Groupify') {
-          //TODO doesn't find playlist for some reason
-          //await spotifyUserApi.unfollowPlaylist(list.id);
-        }
-      } catch(error) {
-        console.log(error);
-      }
-    });
     await delete map[request.body.roomId];
     response.json(200);
   } catch(error) {
@@ -79,18 +115,40 @@ router.get('/login', async function(request, response) {
   }
 });
 
-router.put('/play', async function(request, response) {
+var testDevice;
+var testUser;
+var testList;
+router.post('/play', async function(request, response) {
   try {
     const spotifyUserApi = map[request.body.roomId];
     const token = await spotifyUserApi.refreshAccessToken();
     await spotifyUserApi.setAccessToken(token.body['access_token']);
     const userId = await spotifyUserApi.getMe();
-    const playlist = await spotifyUserApi.getUserPlaylists(userId.body.id);
+    testUser = userId;
+    var playlist = await spotifyUserApi.getUserPlaylists(userId.body.id);
+    const devicesList = await spotifyUserApi.getMyDevices();
+    var deviceId;
+    devicesList.body.devices.forEach(async function(device) {
+      try {
+        if(device.name === 'Groupify'){
+          deviceId = device.id;
+          testDevice = device.id;
+        }
+      } catch(error){
+        console.log(error);
+      }
+    })
     playlist.body.items.forEach(async function(list) {
       try {
-        if(list.name === 'Groupify') {
-          await spotifyUserApi.play({context_uri: "spotify:user:" + userId.body.id + ":playlist:" + list.id});
+        if(list.name === 'Groupify' && firstPlay[request.body.roomId] === false) {
+          // await spotifyUserApi.transferMyPlayback({deviceIds: [deviceId]});
+          testList = list;
+          await spotifyUserApi.play({device_id: deviceId, context_uri: "spotify:user:" + userId.body.id + ":playlist:" + list.id});
+          firstPlay[request.body.roomId] = true;
           response.json(200);
+        }
+        else if(list.name === 'Groupify' && firstPlay[request.body.roomId] === true) {
+          await spotifyUserApi.play();
         }
       } catch(error) {
         console.log(error);
@@ -112,5 +170,78 @@ router.put('/pause', async function(request, response) {
   }
 });
 
+router.post('/getQueue', async function(request, response) {
+  try {
+    const spotifyUserApi = map[request.body.roomId];
+    const token = await spotifyUserApi.refreshAccessToken();
+    await spotifyUserApi.setAccessToken(token.body['access_token']);
+
+    const userId = await spotifyUserApi.getMe();
+    const playlists = await spotifyApi.getUserPlaylists(userId.body.id);
+    var playlistId = '';
+    playlists.body.items.forEach(function(list) {
+        if(list.name === 'Groupify') {
+          playlistId = list.id;
+        }
+    });
+
+    const tracks = await spotifyUserApi.getPlaylistTracks(userId.body.id, playlistId);
+    // console.log(tracks.body.items[0].track);
+    response.json(tracks.body);
+  } catch(error) {
+    console.log(error);
+  }
+});
+
+router.post('/getUserCount', async function(request, response) {
+  console.log(users[request.body.roomId]);
+  response.json(users[request.body.roomId]);
+});
+
+router.post('/vote', async function(request, response) {
+  const roomId = request.body.roomId;
+  votes[roomId]++;
+  console.log(votes[roomId]);
+  if(votes[roomId] >= users[roomId]/2) {
+    await map[roomId].skipToNext();
+    votes[roomId] = 0;
+  }
+  response.json(200);
+});
+
+router.post('/duplicates', async function(req, res) {
+  duplicates[req.body.roomId] = req.body.duplicates;
+  res.json(200);
+});
+
+router.post('/repeatOn', async function(req, res) {
+  const spotifyUserApi = map[req.body.roomId];
+  const token = await spotifyUserApi.refreshAccessToken();
+  await spotifyUserApi.setAccessToken(token.body['access_token']);
+  await spotifyUserApi.setRepeat({state: 'track'});
+  res.json(200);
+});
+
+router.post('/repeatOff', async function(req, res) {
+  const spotifyUserApi = map[req.body.roomId];
+  const token = await spotifyUserApi.refreshAccessToken();
+  await spotifyUserApi.setAccessToken(token.body['access_token']);
+  await spotifyUserApi.setRepeat({state: 'off'});
+  res.json(200);
+});
+
+router.post('/nextTrack', async function(req, res) {
+  const spotifyUserApi = map[req.body.roomId];
+  const token = await spotifyUserApi.refreshAccessToken();
+  await spotifyUserApi.skipToNext();
+});
+
+router.post('/previousTrack', async function(req, res) {
+  const spotifyUserApi = map[req.body.roomId];
+  const token = await spotifyUserApi.refreshAccessToken();
+  await spotifyUserApi.skipToPrevious();
+});
 module.exports = router;
 module.exports.map = map;
+module.exports.users = users;
+module.exports.duplicates = duplicates;
--- routers/join.js
+++ routers/join.js
@@ -2,7 +2,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const bodyParser = require('body-parser');
 const Request = require('request');
-const map = require('./host');
+const host = require('./host');
 
 var spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
@@ -13,15 +13,16 @@ const router = express.Router();
 
 // Send roomId to user FrontEnd
 router.get('/:roomId?', function(request, response, next) {
-  console.log(request.params.roomId);
   response.render('join.ejs', {roomId: request.params.roomId});
 });
 
 // Verify if valid roomId
 router.post('/joinRoom', async function(req, res) {
   try {
-    if(map.map[req.body.roomId])
+    if(host.map[req.body.roomId]) {
+      host.users[req.body.roomId]++;
       res.json({success: true});
+    }
     else
       res.json({success: false});
   } catch(error) {
@@ -49,21 +50,44 @@ router.post('/searchTrack', async function(request, response) {
 **/
 router.post('/addToQueue', async function(request, response) {
   try {
-    const spotifyUserApi = map.map[request.body.roomId];
+    const spotifyUserApi = host.map[request.body.roomId];
     const token = await spotifyUserApi.refreshAccessToken();
     await spotifyUserApi.setAccessToken(token.body['access_token']);
     const userId = await spotifyUserApi.getMe();
     const playlist = await spotifyUserApi.getUserPlaylists(userId.body.id);
+    const duplicate = host.duplicates[request.body.roomId];
     playlist.body.items.forEach(async function(list) {
       try {
         if(list.name === 'Groupify') {
-          await spotifyUserApi.addTracksToPlaylist(userId.body.id, list.id, ["spotify:track:" + request.body.trackId]);
+          // duplicates not allowed
+          if(!duplicate) {
+            const songs = await spotifyUserApi.getPlaylistTracks(userId.body.id, list.id);
+            var found = false;
+            songs.body.items.forEach(async function(song) {
+              if(song.track.id == request.body.trackId)
+                found = true;
+            });
+            if(!found) // only add if not already in playlist
+              await spotifyUserApi.addTracksToPlaylist(userId.body.id, list.id, ["spotify:track:" + request.body.trackId]);
+          }
+          else // duplicates allowed
+            await spotifyUserApi.addTracksToPlaylist(userId.body.id, list.id, ["spotify:track:" + request.body.trackId]);
+          host.map[request.body.roomId] = spotifyUserApi;
           response.json(200);
         }
       } catch(error) {
         console.log(error);
       }
     });
+  } catch(error) {
+    console.log(error);
+  }
+});
+
+router.post('/closeRoom', async function(request, response) {
+  try {
+    host.users[request.body.roomId]--;
+    response.json({success: true});
   } catch(error) {
     console.log(error);
   }
